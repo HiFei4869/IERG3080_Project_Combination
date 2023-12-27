7bef7a6f3fd5e513ddcb842845cac0dbdd610294
HiFei4869 <107867164+HiFei4869@users.noreply.github.com>
2023-12-27 20:51:15 +0800

    Update Movement (2).cs

--- WpfApp2/Movement (2).cs
+++ WpfApp2/Movement (2).cs
@@ -33,31 +33,36 @@ namespace PhysicEngine.Movement
             Radius = Math.Sqrt(mass/Math.PI);
             return Radius;
         }
-		public static void SplitEject(Planet planet, double x_c, double y_c)
-        {
-            //MessageBox.Show($"SplitEject: mouseX = {x_c}, mouseY = {y_c}", "Debug Information", MessageBoxButton.OK, MessageBoxImage.Information);
-            Globals global = new Globals();
+	public static void SplitEject(LinkedList<Planet> planet_list, Planet planet, double x_c, double y_c)
+	{
+	    Globals global = new Globals();
+	    Planet newPlanet = new Planet();
+	    newPlanet = planet;
 
-            double direction = EjectDirection(planet, x_c, y_c);
-            double v_e_x = v_e * Math.Cos(direction);  // x_axis velocity of the ejected mass
-            double v_e_y = v_e * Math.Sin(direction);  // y_axis velocity of the ejected mass
-            double v_x_1 = (planet.mass * planet.v_x - m_e * v_e_x) / (planet.mass - m_e);
-            double v_y_1 = (planet.mass * planet.v_y - m_e * v_e_y) / (planet.mass - m_e);
-            planet.mass = planet.mass - m_e;
-            planet.radius = Mass_To_Radius(planet.mass);
-            planet.v_x = v_x_1;
-            planet.v_y = v_y_1;
+	    double direction = EjectDirection(planet, x_c, y_c);
+	    double v_e_x = v_e * Math.Cos(direction);  // x_axis velocity of the ejected mass
+	    double v_e_y = v_e * Math.Sin(direction);  // y_axis velocity of the ejected mass
+	    double v_x_1 = (planet.mass * planet.v_x - m_e * v_e_x) / (planet.mass - m_e);
+	    double v_y_1 = (planet.mass * planet.v_y - m_e * v_e_y) / (planet.mass - m_e);
 
-            Planet ejectedPlanet = new Planet();
-            ejectedPlanet.mass = m_e;
-            ejectedPlanet.radius = Mass_To_Radius(m_e);
-            ejectedPlanet.v_x = -v_x_1;
-            ejectedPlanet.v_y = -v_y_1;
-            ejectedPlanet.x = planet.x + (planet.radius + birth_distance) * Math.Cos(direction);
-            ejectedPlanet.y = planet.y + (planet.radius + birth_distance) * Math.Cos(direction);
-            ejectedPlanet.color = 0;
-            Globals.planet_list.AddLast(ejectedPlanet);
-        }
+	    newPlanet.mass -= m_e;
+	    newPlanet.radius = Mass_To_Radius(planet.mass);
+	    newPlanet.v_x = v_x_1;
+	    newPlanet.v_y = v_y_1;
+	    planet_list.AddLast(newPlanet);
+	    Collision.DeleteNode(planet.x, planet.y, planet.radius, planet_list);
+	
+	    Planet ejectedPlanet = new Planet();
+	    ejectedPlanet.mass = m_e;
+	    ejectedPlanet.radius = Mass_To_Radius(m_e);
+	    ejectedPlanet.v_x = v_e_x;
+	    ejectedPlanet.v_y = v_e_y;
+	    ejectedPlanet.x = newPlanet.x + (newPlanet.radius + birth_distance) * Math.Cos(direction);
+	    ejectedPlanet.y = newPlanet.y + (newPlanet.radius + birth_distance) * Math.Cos(direction);
+	    ejectedPlanet.color = 0;
+	    ejectedPlanet.id = 0;
+	    Globals.planet_list.AddLast(ejectedPlanet);
+	}
 
         public static double v_e = 2;              // velocity of ejected mass
         public static double m_e = 0.1;              // mass ejected in unit time
